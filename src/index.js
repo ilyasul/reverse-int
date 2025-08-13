5dc89ce43bda918bf0a6bbaec4cea1bf60adf6cc
ilyasul <i.sul95@gmail.com>
2025-08-13 19:48:26 +0300

    feat: completed task with used remainder of division

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let tempN = Math.abs(n);
+  let reversed = 0;
+  while (tempN > 0) {
+    reversed = reversed * 10 + (tempN % 10);
+    tempN = Math.floor(tempN / 10);
+  }
+  if (String(reversed).length < String(n).length) {
+    reversed = `0${String(reversed)}`;
+  }
+  return reversed;
 };
